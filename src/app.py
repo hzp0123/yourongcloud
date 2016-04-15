231e3aeac6ab26e38080a99d9258c7a4d98be5f0
hzp0123 <3c01.cloudsoar@cloudsoar.com>
2016-04-15 14:46:01 +0800

    this is 9

--- src/app.py
+++ src/app.py
@@ -6,8 +6,8 @@ redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-redis.incr('hits')
-    return 'Hello World ! I have been seen %s times.'  % redis.get('hits')
+  redis.incr('hits')
+  return 'Hello World ! I have been seen %s times.' % redis.get('hits')
 if __name__ == "__main__":
-app.run(host="0.0.0.0", debug=True) 
+  app.run(host="0.0.0.0", debug=True)
 
